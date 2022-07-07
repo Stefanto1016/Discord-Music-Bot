d3e4bdef12f3115f6bf4adea426dde9227fd6587
Steven To <76414584+Stefanto1016@users.noreply.github.com>
2022-07-07 14:56:13 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import music
+
+@bot.event
+async def on_message(message):
+  # Ignore messages sent from the bot itself
+  if message.author == await bot.fetch_user(918676569902420018):
+    return
+  
+  
+  await bot.process_commands(message)
+
+# Set up bot with music functionality
+music.setup(bot)
+
+# Actual TOKEN hidden for anonymity 
+bot.run(TOKEN)
